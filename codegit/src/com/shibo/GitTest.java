42c85d82efb1f75bc9eac646cdee87739379dcc0
wushibo <theshibo@163.com>
2020-11-30 23:55:11 +0800

    第三次把本地工程添加到git

--- codegit/src/com/shibo/GitTest.java
+++ codegit/src/com/shibo/GitTest.java
@@ -10,4 +10,8 @@ public class GitTest {
         System.out.println("加油呀 吴士博");
         System.out.println("加油呀 吴士博");
     }
+    public void test01(){
+        System.out.println("加油呀 吴士博");
+        System.out.println("加油呀 吴士博");
+    }
 }
