43723c1efbee5cf2e80610e4ada9b22d87fe59ed
wushibo <theshibo@163.com>
2020-11-30 23:33:12 +0800

    把本地工程添加到git

---
+++ codegit/src/com/shibo/GitTest.java
@@ -0,0 +1,9 @@
+package com.shibo;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HELLO  GIT");
+        System.out.println("HELLO  GIT");
+        System.out.println("HELLO  GIT");
+    }
+}
